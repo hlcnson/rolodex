1499aee15ad02e96fd410d9c9b4197342fc43c18
Laptop Thanh <steelhaw@gmail.com>
2019-09-02 08:22:47 +0700

    Updates

---
+++ precache-manifest.bd2eb16de36c28ff3a7be2d300bc667d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d49465b414bb9e042f9d03e294521e",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "53b9663aac4724151bd1",
+    "url": "/rolodex/static/css/main.1826d457.chunk.css"
+  },
+  {
+    "revision": "607e395712115653f754",
+    "url": "/rolodex/static/js/2.45dda9c7.chunk.js"
+  },
+  {
+    "revision": "53b9663aac4724151bd1",
+    "url": "/rolodex/static/js/main.809cc26c.chunk.js"
+  },
+  {
+    "revision": "65c553987313a6d41c28",
+    "url": "/rolodex/static/js/runtime~main.0f95d8af.js"
+  }
+]);
